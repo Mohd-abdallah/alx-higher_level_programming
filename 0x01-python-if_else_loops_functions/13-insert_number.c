fe44431e4677f3d8ea69a74774a2e107e2ffb1eb
Mohd-abdallah <dawry798@gmail.com>
2023-11-29 19:55:03 +0400

    fixed a typo

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -17,7 +17,7 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 	}
 	new->n = number;
-	new->next = NULL
+	new->next = NULL;
 	if (!node || new->n < node->n)
 	{
 		new->next = node;
